236e1e8c97df07fb39dac98774389a91f0e84d3e
Madelen Foss <113188008+madelenfoss@users.noreply.github.com>
2024-10-24 13:35:03 +0200

    Added logo to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,14 @@
+import airGuitarPlayer from "../assets/images/airguitarlogo.png"
+
 const Header = () => {
   return (
     <div>
       <h1>Vote for your favourite air guitar player</h1>
-      
+      <img 
+         src={airGuitarPlayer} 
+         className="logo"
+         alt="Ari guitar player logo"/>
     </div>
   )
 }
-
 export default Header;
