3b64f60f29e11b3e46954a2c101045ad67f1b73d
Madelen Foss <113188008+madelenfoss@users.noreply.github.com>
2024-10-25 18:01:36 +0200

    Added useState to TotalVotes

--- src/components/TotalVotes.jsx
+++ src/components/TotalVotes.jsx
@@ -1,4 +1,9 @@
+import { useState } from "react"
+import VoteCounter from "./VoteCounter.jsx"
+
 const TotalScore = () => {
+  const [totalScore, setTotalScore] = useState(0);
+
   return (
     <div>
       
