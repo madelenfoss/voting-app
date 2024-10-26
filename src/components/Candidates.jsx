0829e8493a89a2b4d501f581ed53cd009bce3953
Madelen Foss <113188008+madelenfoss@users.noreply.github.com>
2024-10-26 21:14:24 +0200

    Added new classnames to candidate related tags

--- src/components/Candidates.jsx
+++ src/components/Candidates.jsx
@@ -38,25 +38,28 @@ const Candidates = () => {
     <>
       <div className="add-search">
         <form className="addbar" onSubmit={handleSubmit}>
-          <label htmlFor="candidate_name">Candidate name</label>
-          <input 
-            id="candidate_name"
-            type="text" 
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-          <label htmlFor="candidate-image">Upload image</label>
-          <input 
-            id="candidate-image"
-            type="file" 
-            accept="image/*"
-            onChange={(e) => setImage(e.target.files[0])}
-          />
+          <div className="candidate_name">
+            <label htmlFor="candidate">Candidate name</label>
+            <input 
+              id="candidate"
+              type="text" 
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+          <div className="candidate_image">
+            <label htmlFor="image">Upload image</label>
+            <input 
+              id="image"
+              type="file" 
+              accept="image/*"
+              onChange={(e) => setImage(e.target.files[0])}
+            />
+          </div>
           <button onClick={addCandidate}>
             Add candidate
           </button>
         </form>
-
 
         <form className="searchbar">
           <label htmlFor="search"></label>
@@ -69,9 +72,9 @@ const Candidates = () => {
         </form>
       </div>
 
-      <ul className="candidate_list">
+      <ul className="candidate_ul">
         {candidates.map((candidate) => (
-          <li key={candidate.id} className="candidate">
+          <li key={candidate.id} className="candidate_li">
             <div className="candidate-info">
               <h2>{candidate.name}</h2>
               <img src={candidate.image} alt={candidate.name}/>
