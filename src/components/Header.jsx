a7ecd47ebbc068caebf09fc60e2060b7cf25c19f
Madelen Foss <113188008+madelenfoss@users.noreply.github.com>
2024-10-26 22:02:07 +0200

    Commented out image from header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,13 @@
-import airGuitarPlayer from "../assets/images/airguitarlogo.png"
+// import airGuitarPlayer from "../assets/images/airguitarlogo.png"
 
 const Header = () => {
   return (
     <header>
       <h1>Vote for your favourite air guitar player</h1>
-      <img 
+      {/* <img 
          src={airGuitarPlayer} 
          className="logo"
-         alt="Ari guitar player logo"/>
+         alt="Ari guitar player logo"/> */}
     </header>
   )
 }
