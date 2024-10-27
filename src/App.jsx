048dfccd78b039da9e9b852c8b9b3daa75ad8e93
Madelen Foss <113188008+madelenfoss@users.noreply.github.com>
2024-10-27 22:18:24 +0100

    Lifted vote count state to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,26 @@ import Candidates from "./components/Candidates.jsx";
 import TotalVotes from "./components/TotalVotes.jsx";
 import Header from "./components/Header.jsx";
 import Footer from "./components/Footer.jsx";
+import { useState } from "react";
 
 const App = () => {
+  const [totalVotes, setTotalVotes] = useState(0);
+
+  const increaseTotal = () => {
+    setTotalVotes(totalVotes + 1);
+  }
+
+  const decreaseTotal = () => {
+    setTotalVotes(totalVotes - 1);
+  }
+
   return (
     <>
       <Header />
-      <Candidates />
-      <TotalVotes />
+      <main className="main">
+        <Candidates onIncrease={increaseTotal} onDecrease={decreaseTotal} />
+        <TotalVotes totalVotes={totalVotes} />
+      </main>
       <Footer year={2024} />
     </>
   )
